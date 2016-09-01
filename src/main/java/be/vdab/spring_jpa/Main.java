e5f98e1dbe8d57249f8da5cde02d45a4787b03c4
Nancy De Smet <ciek.eu@gmail.com>
2016-09-01 09:28:44 +0200

    nieuwe spring jpa oefening

---
+++ src/main/java/be/vdab/spring_jpa/Main.java
@@ -0,0 +1,7 @@
+package be.vdab.spring_jpa;
+
+/**
+ * Created by tywinlannister on 01/09/16.
+ */
+public class Main {
+}
--- wiring/src/main/java/com/realdolmen/spring/Person.java
+++
@@ -1,7 +0,0 @@
-package com.realdolmen.spring;
-
-/**
- * Created by tywinlannister on 29/08/16.
- */
-public class Person {
-}
